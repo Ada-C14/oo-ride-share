37dc501faa78514f59ac56da6d9ba4c43cbb7d7b
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-29 13:22:44 -0700

    turn start_time and end_time into Time instances

--- lib/trip.rb
+++ lib/trip.rb
@@ -61,8 +61,8 @@ module RideShare
       return self.new(
                id: record[:id],
                passenger_id: record[:passenger_id],
-               start_time: record[:start_time],
-               end_time: record[:end_time],
+               start_time: Time.parse(record[:start_time]),
+               end_time: Time.parse(record[:end_time]),
                cost: record[:cost],
                rating: record[:rating]
              )
