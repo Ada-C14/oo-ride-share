f52151188a3351cb97bf9a8d1b844ef7522dbc44
Lina Do <l.do5147@gmail.com>
2020-09-29 15:01:48 -0700

    created driver class with initialize method and from_csv method

---
+++ lib/driver.rb
@@ -0,0 +1,35 @@
+require_relative 'csv_record'
+
+module RideShare
+  class Driver < CsvRecord
+    attr_reader :name, :vin, :status, :trips
+
+    def initialize(id:, name:, vin:, status:, trips: [])
+      super(id)
+
+      @name = name
+
+      raise ArgumentError, "VIN must be 17 characters long." if vin.length != 17
+      @vin = vin
+
+      if %i[AVAILABLE UNAVAILABLE].include?(status)
+        @status = status
+      else
+        raise ArgumentError, "Invalid status. Must be AVAILABLE or UNAVAILABLE."
+      end
+
+      @trips = trips
+    end
+
+    private
+
+    def self.from_csv(record)
+      return new(
+          id: record[:id],
+          name: record[:name],
+          vin: record[:vin],
+          status: record[:status]
+      )
+    end
+  end
+end
