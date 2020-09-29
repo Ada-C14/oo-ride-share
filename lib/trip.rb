1ae473f22abf5c12098765f36f13241aad9b5362
Alice Boone <alicegbonel@gmail.com>
2020-09-29 15:44:22 -0700

    Changes in trip file

--- lib/trip.rb
+++ lib/trip.rb
@@ -4,12 +4,14 @@ require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
-    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
+    attr_reader :id, :passenger, :passenger_id, :driver, :driver_id, :start_time, :end_time, :cost, :rating
 
     def initialize(
           id:,
           passenger: nil,
           passenger_id: nil,
+          driver: nil,
+          driver_id: nil,
           start_time:,
           end_time:,
           cost: nil,
@@ -26,6 +28,16 @@ module RideShare
 
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
+      end
+
+      if driver
+        @driver = driver
+        @driver_id = driver.id
+
+      elsif driver_id
+        @driver_id = driver_id
+      else
+        raise ArgumentError, 'Drive or Driver ID is required'
       end
 
       @start_time = start_time
@@ -54,9 +66,11 @@ module RideShare
         "rating=#{rating}>"
     end
 
-    def connect(passenger)
+    def connect(passenger, driver)
       @passenger = passenger
       passenger.add_trip(self)
+      @driver = driver
+      driver.add_trip(self)
     end
 
     def duration
