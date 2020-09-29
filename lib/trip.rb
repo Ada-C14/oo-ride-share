f1215383355ba2422909e08904e384676e3263ac
r-spiel <richelle.mehlhaff@gmail.com>
2020-09-29 10:18:06 -0700

    update to trip class method .from_csv to create instances of time for start & end time

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'time'
 
 require_relative 'csv_record'
 
@@ -61,8 +62,8 @@ module RideShare
       return self.new(
                id: record[:id],
                passenger_id: record[:passenger_id],
-               start_time: record[:start_time],
-               end_time: record[:end_time],
+               start_time: Time.parse(record[:start_time]), #"2019-01-24 21:36:19 -0800"
+               end_time: Time.parse(record[:end_time]),
                cost: record[:cost],
                rating: record[:rating]
              )
