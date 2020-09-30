1c0bd42a523e1d721dd304924cab25e9c62fb728
r-spiel <richelle.mehlhaff@gmail.com>
2020-09-29 19:27:48 -0700

    Wave 2 begins!  start of Driver class and uncommented tests

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,45 +1,34 @@
+# frozen_string_literal: true
+
 require_relative 'trip_dispatcher'
 require_relative 'csv_record'
 
-module Rideshare
+#require 'csv'
+
+module RideShare
   class Driver < CsvRecord
+    attr_reader :name, :vin, :status, :trips
 
-    attr_reader
-
-    def initialize(id:, name:, vin:, status:, trips: [])
+    def initialize(id:, name:, vin:, status: :AVAILABLE, trips: [])
       super(id)
 
       @name = name
       @vin = vin
       @status = status
       @trips = trips
+
+      #raise for vin string length
+      # raise error for bad ID
     end
 
-
-    private
-
     def self.from_csv(record)
-      return new(
-          id: record[:id],
-          name: record[:name],
-          vin: record[:vin],
-          status: record[:status],
-
+      new(
+        id: record[:id],
+        name: record[:name],
+        vin: record[:vin],
+        status: record[:status]
       )
     end
 
-    #.from_csv for trip class:
-    # def self.from_csv(record)
-    #   return self.new(
-    #       id: record[:id],
-    #       passenger_id: record[:passenger_id],
-    #       start_time: Time.parse(record[:start_time]), #"2019-01-24 21:36:19 -0800"
-    #       end_time: Time.parse(record[:end_time]),
-    #       cost: record[:cost],
-    #       rating: record[:rating]
-    #   )
-    # end
-
   end
 end
-
