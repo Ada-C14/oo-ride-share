a996c10b1ab660990e52fb59562d3bb80556bb07
roshni-patel <rpatel01@wesleyan.edu>
2020-09-30 14:14:26 -0500

    Added request trip method

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -26,6 +26,31 @@ module RideShare
       return @passengers.find { |passenger| passenger.id == id }
     end
 
+    def request_trip(passenger_id)
+      driver = @drivers.find { |driver| driver.status == :AVAILABLE }
+
+      if driver == nil
+        raise ArgumentError.new("No available drivers")
+      else
+        passenger = find_passenger(passenger_id)
+        id = (@trips.last.id + 1)
+        driver_id = driver.id
+        new_trip = RideShare::Trip.new(
+          id: id,
+          driver_id: driver_id,
+          passenger_id: passenger_id,
+          start_time: Time.now,
+          end_time: nil,
+          cost: nil,
+          rating: nil
+        )
+        new_trip.connect(passenger, driver)
+        @trips << new_trip
+        driver.status = :UNAVAILABLE
+        return new_trip
+      end
+    end
+
     def inspect
       # Make puts output more useful
       return "#<#{self.class.name}:0x#{object_id.to_s(16)} \
@@ -48,5 +73,9 @@ module RideShare
   end
 end
 
-# td = RideShare::TripDispatcher.new
-# pp td.trips
+td = RideShare::TripDispatcher.new
+#pp td.trips
+pp td.find_driver(1)
+pp td.request_trip(40)
+pp td.trips.last.driver.status
+pp td.trips.last.passenger
