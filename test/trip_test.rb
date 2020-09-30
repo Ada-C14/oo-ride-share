4f608fe48a630c60c02783b4d4df96ea556a4c64
Alice D <alicedilanchian@gmail.com>
2020-09-30 17:35:29 -0500

    Wave 0 and Wave 1

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -1,24 +1,26 @@
 require_relative 'test_helper'
 
 describe "Trip class" do
-  describe "initialize" do
-    before do
-      start_time = Time.now - 60 * 60 # 60 minutes
-      end_time = start_time + 25 * 60 # 25 minutes
-      @trip_data = {
+
+  before do
+    start_time = Time.now - 60 * 60 # 60 minutes
+    end_time = start_time + 25 * 60 # 25 minutes
+    @trip_data = {
         id: 8,
         passenger: RideShare::Passenger.new(
-          id: 1,
-          name: "Ada",
-          phone_number: "412-432-7640"
+            id: 1,
+            name: "Ada",
+            phone_number: "412-432-7640"
         ),
         start_time: start_time,
         end_time: end_time,
         cost: 23.45,
         rating: 3
-      }
-      @trip = RideShare::Trip.new(@trip_data)
-    end
+    }
+    @trip = RideShare::Trip.new(@trip_data)
+  end
+
+  describe "initialize" do
 
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
@@ -41,5 +43,37 @@ describe "Trip class" do
         end.must_raise ArgumentError
       end
     end
+
+    # Add a check in Trip#initialize that raises an ArgumentError if the end time
+    # is before the start time, and IN THIS FILE: a corresponding test
+    it "raises an ArgumentError if the end time is before the start time" do
+      # we are exchanging the end and start time, to make them non-valid
+
+      @trip_data[:start_time] = @end_time
+      @trip_data[:end_time] = @start_time
+      expect { RideShare::Trip.new(@trip_data) }.must_raise ArgumentError
+    end
+
+  end
+
+  describe "Duration" do
+    # Add an instance method to the Trip class to calculate the duration of the trip
+    # in seconds, and IN THIS FILE a corresponding test
+    it "calculate the duration of the trip in seconds" do
+      @trip_data[:start_time] = @start_time
+      @trip_data[:end_time] = @end_time
+      expect(@trip.duration).must_equal 25 * 60
+    end
+
   end
 end
+
+
+
+
+
+
+
+
+
+
