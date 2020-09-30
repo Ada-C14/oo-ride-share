8f4d123c247aa8fe5573d852bc7abc0d85c2b28f
marks214 <aimee.oz.x@gmail.com>
2020-09-29 21:01:39 -0700

    updated trip_test.rb wave 1, copy and pasted from ruby mine into VIM - github clone/fork issues, needed to refork and clone files after working on it

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -1,4 +1,104 @@
 require_relative 'test_helper'
+require 'time'
+
+describe "Trip class" do
+  describe "initialize" do
+    before do
+      start_time = Time.now - 60 * 60 # 60 minutes
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(
+          id: 1,
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      @trip = RideShare::Trip.new(@trip_data)
+    end
+
+    it "is an instance of Trip" do
+      expect(@trip).must_be_kind_of RideShare::Trip
+    end
+
+    it "stores an instance of passenger" do
+      expect(@trip.passenger).must_be_kind_of RideShare::Passenger
+    end
+
+    it "stores an instance of driver" do
+      skip # Unskip after wave 2
+      expect(@trip.driver).must_be_kind_of RideShare::Driver
+    end
+
+    it "raises an error for an invalid rating" do
+      [-3, 0, 6].each do |rating|
+        @trip_data[:rating] = rating
+        expect do
+          RideShare::Trip.new(@trip_data)
+        end.must_raise ArgumentError
+      end
+    end
+
+    it "raises an error when the end time is before the start time" do
+      #TODO: check again later, causing errors
+
+      start_time = Time.now + 60 * 60 # 60 minutes
+      end_time = start_time - 25 * 60 # 25 minutes
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(
+          id: 1,
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+
+      expect { RideShare::Trip.new(@trip_data) }.must_raise ArgumentError
+    end
+  end
+
+  describe "duration" do
+    before do
+
+      start_time = Time.now # now
+      end_time = start_time + 60 # add 1 minute
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(
+          id: 1,
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      @trip = RideShare::Trip.new(@trip_data)
+    end
+
+    it "should return a Float" do
+      expect(@trip.duration).must_be_kind_of Float
+    end
+
+    it "should return duration in seconds" do
+      expect(@trip.duration).must_equal 60.0
+    end
+
+    it 'remainder should be zero when divided by 60 (60 seconds in a minute)' do
+      expect((@trip.duration) % 60).must_equal 0
+    end
+  end
+end
+require_relative 'test_helper'
 
 describe "Trip class" do
   describe "initialize" do
