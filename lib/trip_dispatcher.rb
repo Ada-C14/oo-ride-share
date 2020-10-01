d8d8db971886ba03a34fb36e95fd3a59bf489689
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-01 14:55:11 -0700

    raises ArgumentError if there are no available drivers

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -37,23 +37,28 @@ module RideShare
     def request_trip(passenger_id)
       first_available_driver = @drivers.find { |driver| driver.status == :AVAILABLE }
 
-      trip = Trip.new(
-        id: @trips.length + 1,
-        driver_id: first_available_driver.id,
-        passenger_id: passenger_id,
-        start_time: Time.now,
-        end_time: nil,
-        cost: nil,
-        rating: nil
-      )
+      if first_available_driver
+        trip = Trip.new(
+          id: @trips.length + 1,
+          driver_id: first_available_driver.id,
+          passenger_id: passenger_id,
+          start_time: Time.now,
+          end_time: nil,
+          cost: nil,
+          rating: nil
+        )
 
-      passenger = find_passenger(passenger_id)
-      first_available_driver.start_trip(trip)
-      passenger.add_trip(trip)
+        passenger = find_passenger(passenger_id)
+        first_available_driver.start_trip(trip)
+        passenger.add_trip(trip)
 
-      @trips << trip
+        @trips << trip
 
-      return trip
+        return trip
+
+      else
+        raise ArgumentError, "There are no available drivers. Try requesting a trip later."
+      end
     end
 
     private
