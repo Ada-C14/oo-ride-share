707bc8cf6de7677c988bff6f7fa81a9c9ea35d4b
Stacy <lundquist.stacy@gmail.com>
2020-10-01 13:52:47 -0700

    un-skipped driver test, still passing.

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -59,7 +59,6 @@ describe "Trip class" do
     end
 
     it "stores an instance of driver" do
-      skip # Unskip after wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
     end
 
