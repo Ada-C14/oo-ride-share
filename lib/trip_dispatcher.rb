ba3864fae547d52ef12dbe761cdbe9874487dc2e
Jessica Chan <jwinchan@gmail.com>
2020-09-30 17:35:34 -0700

    created find_driver method and updated connect_trips to include driver

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -21,6 +21,11 @@ module RideShare
       return @passengers.find { |passenger| passenger.id == id }
     end
 
+    def find_driver(id)
+      Driver.validate_id(id)
+      return @drivers.find { |driver| driver.id == id}
+    end
+
     def inspect
       # Make puts output more useful
       return "#<#{self.class.name}:0x#{object_id.to_s(16)} \
@@ -34,7 +39,8 @@ module RideShare
     def connect_trips
       @trips.each do |trip|
         passenger = find_passenger(trip.passenger_id)
-        trip.connect(passenger)
+        driver = find_driver(trip.driver_id)
+        trip.connect(passenger, driver)
       end
 
       return trips
