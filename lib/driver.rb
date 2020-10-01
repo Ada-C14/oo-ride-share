919047d4eeb3de39c387e00361794ec2f7bac46b
MonaRahmani <monarahmanimehr@gmail.com>
2020-09-30 18:12:40 -0700

    construtor added for the Driver class

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,7 +1,21 @@
-
+require_relative 'csv_record'
 
 module RideShare
-  class Driver
+  class Driver < CsvRecord
+    attr_reader  :name, :vin, status, trips
 
+    def initialize(id:, name:, vin:, status: :AVAILABLE, trips: [])
+      super(id)
+
+      @name = name
+
+      raise ArgumentError.new("invalid vin #{ vin }") if vin.length != 17
+      @vin = vin
+
+      raise ArgumentError.new("invalid status #{ status }") unless [:AVAILABLE, :UNAVAILABLE].include?(status.to_sym)
+      @status = status.to_sym
+
+      @trips = trips
+    end
   end
 end
