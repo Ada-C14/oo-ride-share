fe734159fe2b91f872fa517fa6f69e53e0b42c30
Leah Scott-Zechlin <scottzec@gmail.com>
2020-10-02 16:27:51 -0700

    request_trip method

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -26,6 +26,39 @@ module RideShare
       return @drivers.find { |driver| driver.id == id }
     end
 
+    def driver_assigned
+      @trips.map(&:trip)
+      return trip.driver if trip.driver.status == :AVAILABLE
+      raise ArgumentError, "No drivers available"
+    end
+
+    def request_trip(passenger_id)
+      driver = driver_assigned
+      start_time = Time.now
+      end_time = nil
+      passenger = find_passenger(passenger_id)
+      trip_data = {
+          id: nil,
+          passenger: passenger,
+          start_time: start_time,
+          end_time: end_time,
+          cost: nil,
+          rating: nil,
+          driver_id: driver.id,
+          driver: driver
+      }
+
+      new_trip_instance = RideShare::Trip.new(trip_data)
+
+      driver.trips << new_trip_instance #  Add the new trip to the collection of trips for that Driver
+      driver.status = :UNAVAILABLE  #          Set the driver's status to :UNAVAILABLE
+      passenger.add_trip(passenger) # Add the Trip to the Passenger's list of Trips
+      @trips << new_trip_instance   #  Add the new trip to the collection of all Trips in TripDispatcher
+
+      return new_trip_instance #  Return the newly created trip
+
+    end
+
     def inspect
       # Make puts output more useful
       return "#<#{self.class.name}:0x#{object_id.to_s(16)} \
