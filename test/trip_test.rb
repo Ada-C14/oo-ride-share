683f203ad590eaaa656173c8fee2d342ebcf4544
marks214 <aimee.oz.x@gmail.com>
2020-10-02 10:49:29 -0700

    added missing edge case for duration method

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -72,6 +72,31 @@ describe "Trip class" do
 
       expect { RideShare::Trip.new(@trip_data) }.must_raise ArgumentError
     end
+
+    it "raises an error when the end time is equal to the start time" do
+
+      start_time = Time.now + 60 * 60 # 60 minutes
+      end_time = start_time
+      @trip_data = {
+        id: 8,
+        driver: RideShare::Driver.new(
+          id: 7,
+          name: "Minnie Driver",
+          vin: "12345678901234567",
+          status: :AVAILABLE),
+        passenger: RideShare::Passenger.new(
+          id: 1,
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+
+      expect { RideShare::Trip.new(@trip_data) }.must_raise ArgumentError
+    end
   end
 
   describe "duration" do
